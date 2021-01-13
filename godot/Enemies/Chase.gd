dcae36262236c39d541acb9a1a5b47799e86cb40
Nathan Lovato <nathan@gdquest.com>
2021-01-13 15:33:21 -0600

    Fix typo in state name

--- godot/Enemies/Chase.gd
+++ godot/Enemies/Chase.gd
@@ -7,7 +7,7 @@ func _enter(_msg := {}) -> void:
 
 func _update(_delta: float) -> void:
 	if owner.target == null:
-		_state_machine.transition_to("Patroll")
+		_state_machine.transition_to("Patrol")
 		return
 	owner.run_direction = sign(owner.target.global_position.x - owner.global_position.x)
 	owner.velocity.x = owner.speed * owner.run_direction
